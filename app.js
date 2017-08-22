92f826806984f76f69b34d4ebce6e04b36c25877
Jason Wei <steve@apple.com>
2017-08-21 17:39:22 -0700

    final commit before presentation

--- app.js
+++ app.js
@@ -1,37 +1,27 @@
-/* $(function() {
-
-$('#buy').on('click', function() {
-  event.preventDefault();
-    alert('Alert');
-    console.log('TEST')
-});
-}); */
-
-
 // Initialize Firebase
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyD7IqUyZZgA4A1JFeGVD9o3pB0iKar9o64",
+    authDomain: "tlgc-8564d.firebaseapp.com",
+    databaseURL: "https://tlgc-8564d.firebaseio.com",
+    projectId: "tlgc-8564d",
+    storageBucket: "tlgc-8564d.appspot.com",
+    messagingSenderId: "444256991482"
+  };
+  firebase.initializeApp(config);
 
-// connect to your Firebase application using your reference URL
+// connect to Firebase
 var database = firebase.database()
 
 // ----------------------------------------------------------------------------
 
 $(document).ready(function () {
   $('#form-inline').submit(function (event) {
-    // by default a form submit reloads the DOM which will subsequently reload all our JS
-    // to avoid this we preventDefault()
     event.preventDefault()
-
-    // grab user message input
     var message = $('#inlineFormInput').val()
-
-    // clear message input (for UX purposes)
     $('#inlineFormInput').val('thanks! we got your email')
-
-    // create a section for messages data in your db
     var messagesReference = database.ref('inlineFormInput');
 
-    // use the set method to save data to the messages
+    // use the set method to save data to messages
     messagesReference.push({
       message: message
     })
--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyD7IqUyZZgA4A1JFeGVD9o3pB0iKar9o64",
-  authDomain: "tlgc-8564d.firebaseapp.com",
-  databaseURL: "https://tlgc-8564d.firebaseio.com",
-  projectId: "tlgc-8564d",
-  storageBucket: "tlgc-8564d.appspot.com",
-  messagingSenderId: "444256991482"
-};
