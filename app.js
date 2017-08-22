799d623e1581298f5f9b7ef1ddf552630ec9e9f8
Jason Wei <steve@apple.com>
2017-08-21 17:35:01 -0700

    setting up config.js

--- app.js
+++ app.js
@@ -9,15 +9,7 @@ $('#buy').on('click', function() {
 
 
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD7IqUyZZgA4A1JFeGVD9o3pB0iKar9o64",
-    authDomain: "tlgc-8564d.firebaseapp.com",
-    databaseURL: "https://tlgc-8564d.firebaseio.com",
-    projectId: "tlgc-8564d",
-    storageBucket: "tlgc-8564d.appspot.com",
-    messagingSenderId: "444256991482"
-  };
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 // connect to your Firebase application using your reference URL
 var database = firebase.database()
--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 var config = {
-  ACCESS_TOKEN = "60c4b769d25b4491a3c30a58f3383daf"
-}
+  apiKey: "AIzaSyD7IqUyZZgA4A1JFeGVD9o3pB0iKar9o64",
+  authDomain: "tlgc-8564d.firebaseapp.com",
+  databaseURL: "https://tlgc-8564d.firebaseio.com",
+  projectId: "tlgc-8564d",
+  storageBucket: "tlgc-8564d.appspot.com",
+  messagingSenderId: "444256991482"
+};
